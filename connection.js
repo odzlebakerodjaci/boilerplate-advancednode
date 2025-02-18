87238afb89fbe9ef90c5430bd8a5f4c869d96912
odzlebakerodjaci <flpmkc@gmail.com>
2025-02-18 18:13:54 +0100

    final

--- connection.js
+++ connection.js
@@ -1,10 +1,24 @@
-// Do not change this file
+https://fork-quick-parcel.glitch.me
+
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 async function main(callback) {
     const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
     const client = new MongoClient(URI);
+
+    async function checkDBConnection() {
+        try {
+            await client.connect(); // Povezivanje na MongoDB
+            console.log('Uspesna konekcija sa bazom!');
+            await client.close(); // Zatvori konekciju nakon provere
+        } catch (err) {
+            console.error('Greška prilikom povezivanja sa bazom:', err);
+        }
+    }
+
+    // Pozivanje funkcije za proveru konekcije
+    await checkDBConnection();
 
     try {
         // Connect to the MongoDB cluster
@@ -16,7 +30,7 @@ async function main(callback) {
     } catch (e) {
         // Catch any errors
         console.error(e);
-        throw new Error('Unable to Connect to Database')
+        throw new Error('Unable to Connect to Database');
     }
 }
 
